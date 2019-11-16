ac56a76832ca3e6e7286469ca9863cdf4ed9575f
Nndou <yp.niu@foxmail.com>
2019-11-16 14:38:31 +1000

    Build navbar and router

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Menu } from "antd";
+import { Menu, Icon } from "antd";
+const { SubMenu } = Menu;
 
 const Navbar = () => {
+  const [current, setCurrent] = useState("home");
+
+  const handleClick = e => {
+    setCurrent(e.key);
+  };
   return (
-    <nav className="navbar text-primary navbar-expand">
+    <nav className="navbar p-0 text-primary navbar-expand">
       <div className="container">
         <Link className="nav-link text-primary" to="/">
-          <h1 className="mb-0">Todo List</h1>
+          <h1 className="navbar__title mb-0">Todo List</h1>
         </Link>
-
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link text-primary" to="/">
+        <Menu
+          className="ml-auto"
+          onClick={handleClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+        >
+          <Menu.Item key="home">
+            <Link to="/">
+              <Icon type="home" />
               Home
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-primary" to="/register">
-              Register
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-primary" to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link text-primary" to="/about">
-              About
-            </Link>
-          </li>
-        </ul>
+          </Menu.Item>
+          <SubMenu
+            title={
+              <span className="submenu-title-wrapper">
+                <Icon type="user" />
+                User
+              </span>
+            }
+          >
+            <Menu.Item key="login">
+              <Link to="/login">
+                <Icon type="login" />
+                Login
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="register">
+              <Link to="/register">
+                <Icon type="user-add" />
+                Register
+              </Link>
+            </Menu.Item>
+          </SubMenu>
+        </Menu>
       </div>
     </nav>
   );
